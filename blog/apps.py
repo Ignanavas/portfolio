c7139da03bebbf09405733f02118dfd767c0da2f
Ignacio <dysrupterwow@gmail.com>
2022-10-11 08:16:19 -0300

    Procfile anadido??

--- blog/apps.py
+++ blog/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
+
+def app(environ, start_response):
+        data = b"Hello, World!\n"
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+        return iter([data])
