b5be0bea7a3c20a86afd38090eb0d8b8f591a069
Ignacio <dysrupterwow@gmail.com>
2022-10-18 00:25:16 -0300

    andando

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,4 @@
 from email.policy import default
-from unittest.util import _MAX_LENGTH
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -9,10 +8,22 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    inicio_fecha = models.DateTimeField(default = timezone.now)
+    fin_fecha= models.DateTimeField(default = timezone.now)
+
     text = models.TextField(default = " ")
     create_date = models.DateTimeField(default = timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)    
+    published_date = models.DateTimeField(blank=True, null=True) 
+    lista_tags = (  ('Exp', 'Experiencia'),
+                    ('Edu','Educacion'),
+                    ('Hab','Habilidades'),
+                    ('Pro','Projectos'),
+                    ('Cer','Certificaciones')
+                )
+    tipo_post = models.CharField(max_length=3, choices=lista_tags ,default=('Exp', 'Experiencia'))   
 
+    
+   
     def publish(self):
         self.published_date = timezone.now()
         self.save()
